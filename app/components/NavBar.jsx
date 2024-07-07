10a38dc23d22e376474d1605c2926cd7d66d53b3
PierreChagnon <pc.chagnon@gmail.com>
2024-07-07 15:35:27 +0200

    thin nav font

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -5,8 +5,8 @@ const NavBar = () => {
     return (
         <nav className="text-gray-800 py-4 px-24 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
-                <Link className="text-2xl font-bold" href='/'>Recipes</Link>
-                <div className="flex space-x-4">
+                <Link className="text-2xl" href='/'>Recipes</Link>
+                <div className="flex space-x-4 font-light">
                     <a href="#table" className="hover:bg-gray-200 duration-200 py-1 px-4 rounded-md">Table</a>
                     <Link href="#abstract" className="hover:bg-gray-200 duration-200 py-1 px-4 rounded-md">Abstract</Link>
                     <a href="#contributors" className="hover:bg-gray-200 duration-200 py-1 px-4 rounded-md">Contributors</a>
